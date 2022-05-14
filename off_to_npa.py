1a10708f3ae886839db529aba138bf14b8556055
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-14 15:38:29 +0430

    Defining meh_loader function

--- off_to_npa.py
+++ off_to_npa.py
@@ -9,3 +9,21 @@ import numpy as np
 from os import walk
 from typing import Generator
 from os.path import abspath, join
+
+
+def mesh_loader(path: str) -> Generator:
+    """
+    List and load all mesh files(.off) in given directory with valid extension
+    :param path: Path to the directory
+    :return: Yields loaded mesh files
+    """
+    path = abspath(path)
+    mesh_pool = list()
+    valid_file_extension = "off"
+    for root, dirs, files in walk(path):
+        for file in files:
+            if file.split(".")[-1] in valid_file_extension:
+                mesh_pool.append(join(root, file))
+    for mesh in mesh_pool:
+        v, f = igl.read_triangle_mesh(mesh, dtypef=float)
+        yield v, f
