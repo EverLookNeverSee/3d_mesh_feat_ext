b81209af5791304de8c0bdf02d4a369e70cb11c8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-14 15:37:43 +0430

    Importing relevant packages

--- off_to_npa.py
+++ off_to_npa.py
@@ -2,3 +2,10 @@
     .off file to numpy array convertor
     @author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+
+import igl
+import numpy as np
+from os import walk
+from typing import Generator
+from os.path import abspath, join
